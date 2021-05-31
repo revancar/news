689ecf1531c6cf21cedc220b629f6d1eea48f5be
revancar00 <rheivant4@gmail.com>
2021-05-31 16:03:47 +0700

    Submission 2 MADE

--- app/src/main/java/com/bangkit/news/ui/home/MainActivity.kt
+++ app/src/main/java/com/bangkit/news/ui/home/MainActivity.kt
@@ -67,28 +67,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun registerBroadCastReceiver(){
-
-        broadcastReceiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-                when (intent.action) {
-                    Intent.ACTION_POWER_CONNECTED -> {
-                        binding.tvPowerStatus.text = getString(R.string.power_connected)
-                    }
-                    Intent.ACTION_POWER_DISCONNECTED -> {
-                        binding.tvPowerStatus.text = getString(R.string.power_disconnected)
-                    }
-                }
-            }
-        }
-
-        val intentFilter = IntentFilter()
-        intentFilter.apply {
-            addAction(Intent.ACTION_POWER_CONNECTED)
-            addAction(Intent.ACTION_POWER_DISCONNECTED)
-        }
-        registerReceiver(broadcastReceiver,intentFilter)
-    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val mInflater =menuInflater
@@ -104,15 +82,5 @@ class MainActivity : AppCompatActivity() {
                 true
             }else -> true
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        registerBroadCastReceiver()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        unregisterReceiver(broadcastReceiver)
     }
 }
